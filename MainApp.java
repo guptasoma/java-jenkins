ede9d104a9524d711346a0a53fc271445f13dd3c
Soma Gupta <somag83@gmail.com>
2022-11-18 15:33:55 +0530

    commit 2

--- MainApp.java
+++ MainApp.java
@@ -1,7 +1,12 @@
 public class MainApp{
 
+		String message;
+
 		public static void main(String[] args){
+			
 			System.out.println("This is Java Jenkins Demo");
+
+			System.out.println("Message : "+message);
 		}
 
 }
